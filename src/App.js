3fb2330652bd88e910ff721f1ea0e9a1e2e698e1
Himanshu Bhardwaz <himanshubhardwaz@Himanshus-MacBook-Air.local>
2024-04-15 13:08:16 +0530

    react demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { JustCallDialer } from "@justcall/justcall-dialer-sdk";
+import { useEffect, useRef } from "react";
 
-function App() {
+const App = () => {
+  const renderRef = useRef(0);
+
+  useEffect(() => {
+    if (!renderRef.current) {
+      const dialer = new JustCallDialer({
+        dialerId: "justcall-dialer",
+        onLogin: (data) => {
+          console.log("Client receiving Logged in data: ", data);
+        },
+        onLogout: () => {
+          console.log("Client receiving Logged out");
+        },
+      });
+
+      dialer.on("call-ringing", (data) => {
+        console.log("Client receiving call-ringing data: ", data);
+      });
+
+      dialer.on("call-answered", (data) => {
+        console.log("Client receiving call-answered data: ", data);
+      });
+
+      dialer.on("call-ended", function (data) {
+        console.log("Client receiving call-ended data: ", data);
+      });
+    }
+
+    renderRef.current++;
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div
+      id="justcall-dialer"
+      style={{ position: "fixed", bottom: "20px", right: "20px", height: 610 }}
+    ></div>
   );
-}
+};
 
 export default App;
